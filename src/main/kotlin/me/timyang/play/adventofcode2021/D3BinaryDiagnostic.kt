52570e92d37c0ac8b94358c87621d7775c8bf240
timyang <timyang2016@foxmail.com>
2022-01-03 00:57:26 +0800

    d3 part2

--- src/main/kotlin/me/timyang/play/adventofcode2021/D3BinaryDiagnostic.kt
+++ src/main/kotlin/me/timyang/play/adventofcode2021/D3BinaryDiagnostic.kt
@@ -1,10 +1,80 @@
 package me.timyang.play.adventofcode2021
 
+import java.lang.StringBuilder
+
 /**
  * @link https://adventofcode.com/2021/day/3
  */
+fun ans1(inputRecs: List<String>): Int {
+  val bucketSize = inputRecs[0].toCharArray().size
+  val arr2d = Array(bucketSize) {
+    Array(2) {0}
+  }
+  inputRecs.forEach {
+    it.toCharArray().forEachIndexed {
+        index, c -> arr2d[index][c.digitToInt()]++
+    }
+  }
+  var gammaRte = 0
+  var epsilonRte = 0
+  arr2d.forEach {
+    val gIndex = it.indices.maxByOrNull { bit -> it[bit] }!!
+    gammaRte = gammaRte shl 1 or gIndex
+    epsilonRte = epsilonRte shl 1 or ((gIndex + 1) % 2)
+  }
+
+  println("gamma rate: $gammaRte, bin: ${gammaRte.toString(2)}")
+  println("epsilon rate: $epsilonRte, bin: ${epsilonRte.toString(2)}")
+
+  return gammaRte * epsilonRte
+}
+
+fun ans2(inputRecs: List<String>): Int {
+
+  fun oxygenRating(): Int {
+    val patternStr = StringBuilder("")
+    for (i in inputRecs[0].toCharArray().indices) {
+      val arr = IntArray(2){0}
+      val filtered = inputRecs.filter { it.startsWith(patternStr.toString()) }
+      filtered.forEach {
+        arr[it[i].digitToInt()]++
+      }
+      if (filtered.size == 1) {
+        return filtered.single().toInt(2)
+      }
+
+      val gIndex = if (arr[0] == arr[1]) 1 else arr.indices.maxByOrNull { bit -> arr[bit] }!!
+      patternStr.append(gIndex)
+    }
+    return patternStr.toString().toInt(2)
+  }
+  fun co2ScrubberRate(): Int {
+    val patternStr = StringBuilder("")
+    for (i in inputRecs[0].toCharArray().indices) {
+      val arr = IntArray(2){0}
+      val filtered = inputRecs.filter { it.startsWith(patternStr.toString()) }
+      filtered.forEach {
+        arr[it[i].digitToInt()]++
+      }
+      if (filtered.size == 1) {
+        return filtered.single().toInt(2)
+      }
+
+      val gIndex = if (arr[0] == arr[1]) 0 else arr.indices.minByOrNull { bit -> arr[bit] }!!
+      patternStr.append(gIndex)
+    }
+    return patternStr.toString().toInt(2)
+  }
+  val oxygenGenRating = oxygenRating()
+  val co2ScrubberRate = co2ScrubberRate()
+  println("oxygen generator rating: $oxygenGenRating, bin: ${oxygenGenRating.toString(2)}")
+  println("CO2 Scrubber rating: $co2ScrubberRate, bin: ${co2ScrubberRate.toString(2)}")
+
+  return oxygenGenRating * co2ScrubberRate
+}
 
 fun main() {
+
 //  val inputRecs = """00100
 //11110
 //10110
@@ -19,24 +89,6 @@ fun main() {
 //01010
 //  """.trimIndent().split("\n")
   val inputRecs = Utils.readFileLines("src/main/resources/adventofcode/d3_input")
-  val bucketSize = inputRecs[0].toCharArray().size
-  val arr2d = Array(bucketSize) {
-    Array(2) {0}
-  }
-  inputRecs.forEach {
-    it.toCharArray().forEachIndexed {
-      index, c -> arr2d[index][c.digitToInt()]++
-    }
-  }
-  var gammaRte = 0
-  var epsilonRte = 0
-  arr2d.forEach {
-    val gIndex = it.indices.maxByOrNull { bit -> it[bit] }!!
-    gammaRte = gammaRte shl 1 or gIndex
-    epsilonRte = epsilonRte shl 1 or ((gIndex + 1) % 2)
-  }
-  println("gamma rate: $gammaRte, bin: ${gammaRte.toString(2)}")
-  println("epsilon rate: $epsilonRte, bin: ${epsilonRte.toString(2)}")
-
-  println("power consumption: ${gammaRte * epsilonRte}")
+  println("power consumption: ${ans1(inputRecs)}")
+  println("life support rating: ${ans2(inputRecs)}")
 }
